5fca90190fed4aab674891eb9c6b91bb1d495f8f
darthmegatron <roblouiscollins@gmail.com>
2018-10-23 01:01:36 -0400

    Update

--- testing_env.sh
+++ testing_env.sh
@@ -2,10 +2,71 @@
 
 # This script makes an environment for testing file manipulation on the user's desktop.
 
-mkdir ~/Desktop/Testing
-cd ~/Desktop/Testing
+mkdir ./Testing
+cd ./Testing
+#  mkdir ~/Desktop/Testing
+#  cd ~/Desktop/Testing
 touch file1.txt
 touch file2.txt
 touch file3.txt
 touch file4.txt
-echo 'error' >> file1.txt
+echo '1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+error' > file1.txt
+echo 'error
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30' > file2.txt
--- zipper.sh
+++ zipper.sh
@@ -2,12 +2,27 @@
 
 # This script will zip all files in the current directory as long as it does not contain any errors.
 
-files="$(ls --ignore=*tar.gz | sort -hr | head -n 20)"
+
+files="$(ls --ignore=*tar.gz | sort -h | head -n 20)"
 today="$(date +%Y%m%d)"
 
 for item in $files; do
-	tail -n 20 $item | grep -iq error
+    tail -n 20 $item | grep -iq error
 	if [ $(echo $?) == 0 ]
-		then echo '---------- Error found on' $item ' ----------'
+		then
+		    echo '---------- Error found on' $item ' ----------'
+		    echo #  Console spacing
+		    grep -i error $item
+		    echo #  Console spacing
+		    echo '*** Attention: This file will not be zipped ***'
+		    echo #  Console spacing
+		    echo '-----------------------------------------------'
+
+	else
+	    tar -zvcf $item-$today.tar.gz $item && rm -f $item
+	    echo $item 'has been successfully zipped.'
+	    echo #  Console spacing
+
 	fi
+
 done
